d1a5c33248951a170c1f4ca3116faa47bb156e1f
bishal.rumba007 <bishal.rumba007@gmail.com>
2023-10-25 23:17:35 +0545

    about me complete, portfolio create

--- lib/src/responsive/view/mobile/mobile_scaffold.dart
+++ lib/src/responsive/view/mobile/mobile_scaffold.dart
@@ -3,6 +3,8 @@ import 'package:flutter_svg/svg.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:portfolio_web_app/src/themes/color.dart';
+import 'package:portfolio_web_app/src/widgets/export.dart';
+
 
 class MobileScaffoldView extends StatefulWidget {
   const MobileScaffoldView({super.key});
@@ -37,8 +39,7 @@ class _MobileScaffoldViewState extends State<MobileScaffoldView> {
                     padding: const EdgeInsets.all(14),
                     decoration: BoxDecoration(
                         color: AppColor.avatarBackground,
-                        borderRadius: BorderRadius.circular(0.3 * w)
-                    ),
+                        borderRadius: BorderRadius.circular(0.3 * w)),
                     child: SvgPicture.asset(
                       "assets/icons/linkedin.svg",
                     ),
@@ -48,8 +49,7 @@ class _MobileScaffoldViewState extends State<MobileScaffoldView> {
                     width: 0.16 * w,
                     decoration: BoxDecoration(
                         color: AppColor.avatarBackground,
-                        borderRadius: BorderRadius.circular(30)
-                    ),
+                        borderRadius: BorderRadius.circular(30)),
                     child: SvgPicture.asset(
                       "assets/icons/github.svg",
                       colorFilter: ColorFilter.mode(
@@ -65,9 +65,8 @@ class _MobileScaffoldViewState extends State<MobileScaffoldView> {
                     height: 0.16 * w,
                     width: 0.16 * w,
                     decoration: BoxDecoration(
-                      color: AppColor.avatarBackground,
-                      borderRadius: BorderRadius.circular(0.3 * w)
-                    ),
+                        color: AppColor.avatarBackground,
+                        borderRadius: BorderRadius.circular(0.3 * w)),
                     child: SvgPicture.asset(
                       "assets/icons/medium.svg",
                     ),
@@ -75,9 +74,13 @@ class _MobileScaffoldViewState extends State<MobileScaffoldView> {
                 ],
               ),
             ),
-            SizedBox(height: 0.006 * h,),
+            SizedBox(
+              height: 0.006 * h,
+            ),
             SvgPicture.asset("assets/icons/hero.svg"),
-            SizedBox(height: 0.02 * h,),
+            SizedBox(
+              height: 0.02 * h,
+            ),
             Align(
               alignment: Alignment.bottomLeft,
               child: Container(
@@ -91,54 +94,62 @@ class _MobileScaffoldViewState extends State<MobileScaffoldView> {
                 ),
                 child: Row(
                   children: [
-                    const Icon(Icons.back_hand, color: Colors.yellow,),
-                    SizedBox(width: 0.02 * w,),
-                    Text(
-                      'Greetings',
-                      style: Theme.of(context).textTheme.titleLarge?.copyWith(
-                        fontSize: 20,
-                        fontWeight: FontWeight.w900,
-                        color: Theme.of(context).colorScheme.primary.withOpacity(0.7)
-                      )
+                    const Icon(
+                      Icons.back_hand,
+                      color: Colors.yellow,
+                    ),
+                    SizedBox(
+                      width: 0.02 * w,
+                    ),
+                    Text('Greetings',
+                        style: Theme.of(context).textTheme.titleLarge?.copyWith(
+                            fontSize: 20,
+                            fontWeight: FontWeight.w900,
+                            color: Theme.of(context)
+                                .colorScheme
+                                .primary
+                                .withOpacity(0.7),
+                        ),
                     ),
                   ],
                 ),
               ),
             ),
-            SizedBox(height: 0.016 * h,),
+            SizedBox(
+              height: 0.016 * h,
+            ),
             Align(
               alignment: Alignment.bottomLeft,
               child: Padding(
                 padding: EdgeInsets.only(left: 0.10 * w),
-                child: Text(
-                  "Bishal \nRumba",
-                  style: Theme.of(context).textTheme.displayLarge?.copyWith(
-                    fontWeight: FontWeight.w900,
-                    fontSize: 45
-                  )
+                child: Text("Bishal \nRumba",
+                    style: Theme.of(context).textTheme.displayLarge?.copyWith(
+                        fontWeight: FontWeight.w900,
+                        fontSize: 45,
+                        height: 0.9,),
                 ),
               ),
             ),
-            SizedBox(height: 0.01 * h,),
+            SizedBox(
+              height: 0.01 * h,
+            ),
             Align(
               alignment: Alignment.bottomLeft,
               child: Padding(
                 padding: EdgeInsets.only(left: 0.10 * w),
-                child: Text(
-                  "Flutter developer · UI/UX Practitioner",
-                  style: Theme.of(context).textTheme.bodyMedium?.copyWith(
-                    color: Colors.white.withOpacity(0.5)
-                  )
-                ),
+                child: Text("Flutter developer · UI/UX Practitioner",
+                    style: Theme.of(context).textTheme.bodyMedium),
               ),
             ),
-            SizedBox(height: 0.012 * h,),
+            SizedBox(
+              height: 0.012 * h,
+            ),
             Padding(
               padding: EdgeInsets.only(left: 0.08 * w),
               child: Row(
                 children: [
                   TextButton(
-                    onPressed: (){},
+                    onPressed: () {},
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
@@ -147,28 +158,54 @@ class _MobileScaffoldViewState extends State<MobileScaffoldView> {
                           style: GoogleFonts.raleway(
                             fontWeight: FontWeight.w600,
                             fontSize: 14,
-                            color: Theme.of(context).colorScheme.primary.withOpacity(0.5),
+                            color: Theme.of(context)
+                                .colorScheme
+                                .primary
+                                .withOpacity(0.5),
                           ),
                         ),
-                        SizedBox(width: 0.02 * w,),
-                        Icon(Icons.file_download_outlined, size: 22, color: Theme.of(context).colorScheme.primary.withOpacity(0.5),)
+                        SizedBox(
+                          width: 0.02 * w,
+                        ),
+                        Icon(
+                          Icons.file_download_outlined,
+                          size: 22,
+                          color: Theme.of(context)
+                              .colorScheme
+                              .primary
+                              .withOpacity(0.5),
+                        )
                       ],
                     ),
                   ),
-                  SizedBox(width: 0.05 * w,),
+                  SizedBox(
+                    width: 0.05 * w,
+                  ),
                   OutlinedButton(
-                    onPressed: (){},
+                    onPressed: () {},
                     child: Row(
                       mainAxisAlignment: MainAxisAlignment.center,
                       children: [
-                        Icon(FontAwesomeIcons.viber, size: 22, color: Theme.of(context).colorScheme.primary.withOpacity(0.5),),
-                        SizedBox(width: 0.02 * w,),
+                        Icon(
+                          FontAwesomeIcons.viber,
+                          size: 22,
+                          color: Theme.of(context)
+                              .colorScheme
+                              .primary
+                              .withOpacity(0.5),
+                        ),
+                        SizedBox(
+                          width: 0.02 * w,
+                        ),
                         Text(
                           "Contact Me",
                           style: GoogleFonts.raleway(
                             fontWeight: FontWeight.w600,
                             fontSize: 14,
-                            color: Theme.of(context).colorScheme.primary.withOpacity(0.5),
+                            color: Theme.of(context)
+                                .colorScheme
+                                .primary
+                                .withOpacity(0.5),
                           ),
                         ),
                       ],
@@ -177,10 +214,18 @@ class _MobileScaffoldViewState extends State<MobileScaffoldView> {
                 ],
               ),
             ),
-            SizedBox(height: 0.078 * h,),
-            const YearsSection(),
-            SizedBox(height: 0.058 * h,),
-            AboutMeSection(),
+            SizedBox(
+              height: 0.078 * h,
+            ),
+            const ExperienceSection(),
+            SizedBox(
+              height: 0.078 * h,
+            ),
+            const AboutMeSection(),
+            SizedBox(
+              height: 0.05 * h,
+            ),
+            PortfolioSection()
             // const SizedBox(height: 400,),
           ],
         ),
@@ -188,135 +233,3 @@ class _MobileScaffoldViewState extends State<MobileScaffoldView> {
     );
   }
 }
-
-
-class YearsSection extends StatelessWidget {
-  const YearsSection({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    final h = size.height;
-    final w = size.width;
-    return Column(
-      crossAxisAlignment: CrossAxisAlignment.center,
-      children: [
-        Container(
-          height: 0.28 * h,
-          width: 0.68 * w,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(16),
-            border: Border.all(
-              color: Theme.of(context).colorScheme.primary.withOpacity(0.5),
-              width: 1
-            )
-          ),
-          child: Column(
-            children: [
-              SizedBox(height: 0.08 * h,),
-              Icon(
-                FontAwesomeIcons.code,
-                size: 30,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              SizedBox(height: 0.02 * h,),
-              Text(
-                '2 years as',
-                style: Theme.of(context).textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.w400),
-              ),
-              Text(
-                'Programmer',
-                style: Theme.of(context).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-        ),
-        SizedBox(height: 0.04 * h,),
-        Container(
-          height: 0.28 * h,
-          width: 0.68 * w,
-          decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(16),
-              border: Border.all(
-                  color: Theme.of(context).colorScheme.primary.withOpacity(0.5),
-                  width: 1
-              )
-          ),
-          child: Column(
-            children: [
-              SizedBox(height: 0.08 * h,),
-              Icon(
-                FontAwesomeIcons.figma,
-                size: 30,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              SizedBox(height: 0.02 * h,),
-              Text(
-                '2 years as',
-                style: Theme.of(context).textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.w400),
-              ),
-              Text(
-                'UI|UX Practitioner',
-                style: Theme.of(context).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-        ),
-        SizedBox(height: 0.04 * h,),
-        Container(
-          height: 0.28 * h,
-          width: 0.68 * w,
-          decoration: BoxDecoration(
-            color: Theme.of(context).colorScheme.primary.withOpacity(0.1),
-              borderRadius: BorderRadius.circular(16),
-              border: Border.all(
-                  color: Theme.of(context).colorScheme.primary.withOpacity(0.1),
-                  width: 1
-              )
-          ),
-          child: Column(
-            children: [
-              SizedBox(height: 0.08 * h,),
-              Icon(
-                FontAwesomeIcons.archway,
-                size: 30,
-                color: Theme.of(context).colorScheme.primary,
-              ),
-              SizedBox(height: 0.02 * h,),
-              Text(
-                '2 plus years of',
-                style: Theme.of(context).textTheme.bodyLarge?.copyWith(fontWeight: FontWeight.w400),
-              ),
-              Text(
-                'Experience',
-                style: Theme.of(context).textTheme.headlineSmall?.copyWith(fontWeight: FontWeight.bold),
-              ),
-            ],
-          ),
-        ),
-      ],
-    );
-  }
-}
-
-
-class AboutMeSection extends StatelessWidget {
-  const AboutMeSection({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    final size = MediaQuery.of(context).size;
-    final h = size.height;
-    final w = size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 18),
-      child: Column(
-        children: [
-          Image.asset("assets/me.png", height: 300, width: 300,),
-        ],
-      ),
-    );
-  }
-}
-
-
