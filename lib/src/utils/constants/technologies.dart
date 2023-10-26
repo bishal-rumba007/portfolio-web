32a4e8276a23265a964bf7599b8279fec3fbcf9d
bishal.rumba007 <bishal.rumba007@gmail.com>
2023-10-26 23:53:21 +0545

    create technology list

---
+++ lib/src/utils/constants/technologies.dart
@@ -0,0 +1,15 @@
+
+
+
+import 'package:flutter/cupertino.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
+
+List<String> techList = [
+  "assets/icons/flutter.svg",
+  "assets/icons/dart.svg",
+  "assets/icons/js.svg",
+  "assets/icons/java.svg",
+  "assets/icons/firebase.svg",
+  "assets/icons/git.svg",
+  "assets/icons/figma.svg",
+];
