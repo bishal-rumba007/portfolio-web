e2eafcd6f9b44b0465715ca392c99ee301eec2a0
bishal.rumba007 <bishal.rumba007@gmail.com>
2023-10-26 23:53:35 +0545

    create project list

---
+++ lib/src/utils/constants/project_list.dart
@@ -0,0 +1,36 @@
+
+import 'package:portfolio_web_app/src/data/models/project_model.dart';
+
+List<Project> myProjects = [
+  Project(
+    title: "E-book app",
+    description: "Sajha Prakasan App (Subscription model)",
+    appUrl: "https://play.google.com/store/apps/details?id=com.searchtech.sajhabooks&hl=en&gl=US",
+    language: ["Dart", "Flutter"],
+    imgUrl: "assets/images/sajha.svg",
+    projectType: "Client"
+  ),
+  Project(
+    title: "Accounting App",
+    description: "Khata System app made for a client ",
+    appUrl: "https://play.google.com/store/apps/details?id=com.searchtech.khatasystem",
+    language: ["Dart", "Flutter"],
+    imgUrl: "assets/images/sajha.svg",
+    projectType: "Client"
+  ),
+  Project(
+    title: "Hacker News App",
+    description: "Hacker News API integration \n and fetch Top stories, comments & Web View",
+    appUrl: "https://play.google.com/store/apps/details?id=com.searchtech.khatasystem",
+    language: ["Dart", "Flutter"],
+    imgUrl: "assets/images/sajha.svg",
+    projectType: "Personal"
+  ),
+  Project(
+      title: "Ecommerce App",
+      description: "Simple Ecommerce app built using Nodejs backend and Mongodb for database",
+      language: ["Flutter", "MongoDb", "NodeJs"],
+      imgUrl: "assets/images/sajha.svg",
+      projectType: "Personal"
+  ),
+];
