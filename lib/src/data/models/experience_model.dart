65e09d3da80a7ed119ccbaa427ad888480c7f993
bishal.rumba007 <bishal.rumba007@gmail.com>
2023-10-27 11:44:06 +0545

    create experiene data, list

---
+++ lib/src/data/models/experience_model.dart
@@ -0,0 +1,20 @@
+
+
+class ExperienceModel{
+  String position;
+  String title;
+  String description;
+  String from;
+  String to;
+  String duration;
+
+  ExperienceModel({
+    required this.position,
+    required this.title,
+    required this.description,
+    required this.from,
+    required this.to,
+    required this.duration
+});
+  
+}
---
+++ lib/src/utils/constants/experience_list.dart
@@ -0,0 +1,24 @@
+
+
+
+
+import 'package:portfolio_web_app/src/data/models/experience_model.dart';
+
+List<ExperienceModel> experience = [
+  ExperienceModel(
+      position: "Flutter Developer",
+      title: "Flutter developer at Search Technology",
+      description: "Built mobile app for clients and tailored app for search tech",
+      from: "Dec 2022",
+      to: "Jul 2023",
+      duration: "8 months",
+  ),
+  ExperienceModel(
+    position: "Mobile Application Developer",
+    title: "Mobile application developer at TicketSewa",
+    description: "Building ticketing application for ticketsewa",
+    from: "Jul 2023",
+    to: "Present",
+    duration: "4 months",
+  ),
+];
