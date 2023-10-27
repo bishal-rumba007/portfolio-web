b9b2813287b3376aa79d63c53155d8a3c88282c8
bishal.rumba007 <bishal.rumba007@gmail.com>
2023-10-27 11:45:11 +0545

    update export with experience section

--- lib/src/widgets/export.dart
+++ lib/src/widgets/export.dart
@@ -2,3 +2,4 @@ export 'package:portfolio_web_app/src/widgets/experience.dart';
 export 'package:portfolio_web_app/src/widgets/about_me.dart';
 export 'package:portfolio_web_app/src/widgets/portfolio.dart';
 export 'package:portfolio_web_app/src/widgets/skills.dart';
+export 'package:portfolio_web_app/src/widgets/careers.dart';
