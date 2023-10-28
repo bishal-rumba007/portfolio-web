4279859de13483aadfa3a5909be75ea25f404b31
bishal.rumba007 <bishal.rumba007@gmail.com>
2023-10-28 11:49:08 +0545

    email string, copyright string add

--- lib/src/utils/constants/app_string.dart
+++ lib/src/utils/constants/app_string.dart
@@ -3,6 +3,8 @@
 
 class AppString{
 
+  static const email = "bishal.rumba007@gmail.com";
+  static const copyRightText = "Copyright © Bishal Rumba · 2023";
   static const String aboutMe = "I am a passionate mobile application developer specializing in the versatile Flutter framework.\n"
   "I excel at crafting cutting-edge mobile applications, harnessing the potential of Flutter technology. I bring to the table a deep understanding of programming concepts, which empowers me to tackle projects of moderate complexity with unwavering determination. \n"
   "In addition to my coding prowess, I'm driven by an innate talent for problem-solving and a flair for creating seamless and intuitive UI/UX designs. With years of industry experience, I've successfully delivered a series of notable projects, continually striving to surpass my previous achievements and shape the future of mobile application development.";
